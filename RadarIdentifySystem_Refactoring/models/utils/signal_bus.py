15a7ad242d2d3456d4cea58431fd1a23e01f12ab
Zhosolune <1564228136@qq.com>
2025-09-17 14:33:13 +0800

    feat(信号总线): 添加参数配置信号总线类
    新增ParamsConfigSignalBus类用于管理参数配置相关信号，实现组件间解耦通信

--- RadarIdentifySystem_Refactoring/models/utils/signal_bus.py
+++ RadarIdentifySystem_Refactoring/models/utils/signal_bus.py
@@ -3,7 +3,7 @@ from PyQt6.QtCore import QObject, pyqtSignal
 
 
 class MainWindowSignalBus(QObject):
-    """信号总线类
+    """主窗口信号总线类
     
     用于管理应用程序中的主窗口类的全局信号，实现组件间的解耦通信。
     """
@@ -16,5 +16,16 @@ class MainWindowSignalBus(QObject):
     settingInterfaceRestartSig = pyqtSignal()
 
 
+class ParamsConfigSignalBus(QObject):
+    """参数配置信号总线类
+    
+    用于管理应用程序中的参数配置类的全局信号，实现组件间的解耦通信。
+    """
+
+    # 参数配置信号
+    paramChanged = pyqtSignal(str, object)  # (param_name, new_value)
+
+
 # 创建信号总线实例
 mw_signalBus = MainWindowSignalBus()
+pc_signalBus = ParamsConfigSignalBus()
